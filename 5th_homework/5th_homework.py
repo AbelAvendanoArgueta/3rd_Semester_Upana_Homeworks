4624dabdc377b6b94475aac679f495ee80f4731b
Lord_Medusin <jaegerfer7avendano@gmail.com>
2023-04-28 01:49:12 -0600

    base establecida, donde se solicitan valores a, b, c de la ecuacion cuadratica ax^2 + bx + c y el rango de los  parametros de x que se quiere visualizar en consola

--- 5th_homework/5th_homework.py
+++ 5th_homework/5th_homework.py
@@ -19,7 +19,7 @@ import os
 
 # función para imprimir encabezados centrados
 def print_header(text):
-    
+
     # La función get_terminal_size() devuelve un objeto 
     # os.terminal_size que contiene el tamaño actual de la terminal.
     terminal_size = os.get_terminal_size().columns
@@ -38,3 +38,33 @@ print_header("Abel Fernando Avendaño Argueta 000127599")
 
 # Saludo
 print("\n\n         Bienvenido a este programa! \n\n")
+
+def terminal_size():
+    """Obtiene el tamaño actual de la terminal."""
+    columns, rows = os.get_terminal_size()
+    return columns, rows
+
+# Solicitar al usuario los parámetros de la ecuación cuadrática
+a = float(input("Ingrese el valor de a: "))
+b = float(input("Ingrese el valor de b: "))
+c = float(input("Ingrese el valor de c: "))
+x_min = float(input("Ingrese el valor mínimo de x: "))
+x_max = float(input("Ingrese el valor máximo de x: "))
+
+# Definir la altura y ancho de la matriz de salida
+ancho, altura = terminal_size()
+
+# Recorrer cada fila y columna de la matriz
+for y_espacio in range(altura):
+    for x_espacio in range(ancho):
+        x = x_espacio * (x_max - x_min) / ancho + x_min
+        y = altura - 1 - y_espacio
+        if abs(y - int(a * x**2 + b * x + c)) < 1:
+            print("*", end="")
+        elif x_espacio == int(x_min * ancho / (x_max - x_min)):
+            print("|", end="")
+        elif y_espacio == int(altura / 2):
+            print("-", end="")
+        else:
+            print(" ", end="")
+    print()  # Imprimir un salto de línea al final de cada fila
